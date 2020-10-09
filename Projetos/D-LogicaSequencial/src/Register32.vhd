f3f2ea99d3acc101e942bfa4d3a97e4c41d7322e
leonardodma <leonardodma@al.insper.edu.br>
2020-10-08 22:11:54 -0300

    testes passando - falta rtl

--- Projetos/D-LogicaSequencial/src/Register32.vhd
+++ Projetos/D-LogicaSequencial/src/Register32.vhd
@@ -27,5 +27,23 @@ architecture arch of Register32 is
 
 begin
 
+	 -- Implementação vem aqui!
+	 Reg16_1: Register16
+	 port map(
+	   input => input (31 downto 16),
+	   output => output(31 downto 16),
+	   load => load,
+	   clock => clock
+	 );
+
+	 Reg16_2: Register16
+	 port map(
+	   input => input (15 downto 0),
+	   output => output(15 downto 0),
+	   load => load,
+	   clock => clock
+	 );
+
+
 
 end architecture;
