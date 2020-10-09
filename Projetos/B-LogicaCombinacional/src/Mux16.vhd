57c1fb8d04e33548e1a97577c4bf64e4a7282381
delattre1 <daniieeldelattre@gmail.com>
2020-10-09 14:56:43 -0300

    ram 512 com rtl e wave

--- Projetos/B-LogicaCombinacional/src/Mux16.vhd
+++ Projetos/B-LogicaCombinacional/src/Mux16.vhd
@@ -11,7 +11,21 @@ end entity;
 
 architecture arch of Mux16 is
 begin
-
-	q <= (a AND NOT sel) OR (b AND sel);
+		q(0)  <=  (not sel and a(0 )) or (sel and b(0 ));
+		q(1)  <=  (not sel and a(1 )) or (sel and b(1 ));
+		q(2)  <=  (not sel and a(2 )) or (sel and b(2 ));
+		q(3)  <=  (not sel and a(3 )) or (sel and b(3 ));
+		q(4)  <=  (not sel and a(4 )) or (sel and b(4 ));
+		q(5)  <=  (not sel and a(5 )) or (sel and b(5 ));
+		q(6)  <=  (not sel and a(6 )) or (sel and b(6 ));
+		q(7)  <=  (not sel and a(7 )) or (sel and b(7 ));
+		q(8)  <=  (not sel and a(8 )) or (sel and b(8 ));
+		q(9)  <=  (not sel and a(9 )) or (sel and b(9 ));
+		q(10) <=  (not sel and a(10)) or (sel and b(10));
+		q(11) <=  (not sel and a(11)) or (sel and b(11));
+		q(12) <=  (not sel and a(12)) or (sel and b(12));
+		q(13) <=  (not sel and a(13)) or (sel and b(13));
+		q(14) <=  (not sel and a(14)) or (sel and b(14));
+		q(15) <=  (not sel and a(15)) or (sel and b(15));
 
 end architecture;
