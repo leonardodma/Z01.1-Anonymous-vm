f1e0596aa8e7fa6c40cdc3a0cf07a152c51036dd
Delattre1 <daniieeldelattre@gmail.com>
2020-11-02 19:02:38 -0300

    dani controlUnit testado e funcionando

--- Projetos/F-Computador/src/rtl/ControlUnit.vhd
+++ Projetos/F-Computador/src/rtl/ControlUnit.vhd
@@ -29,6 +29,31 @@ end entity;
 architecture arch of ControlUnit is
 
 begin
+	loadD 	<=     instruction(17) and instruction(4);
+	loadM 	<=     instruction(17) and instruction(5);
+	loadA 	<= (not instruction(17) or  instruction(3));
+
+	muxALUI_A <= not instruction(17);
+	muxAM	<=	instruction(17) and instruction(13);
+	
+	zx 	<=	instruction(17) and instruction(12);
+	nx	<=	instruction(17) and instruction(11);
+	zy	<=	instruction(17) and instruction(10);
+	ny	<=	instruction(17) and instruction(9);
+	f 	<=	instruction(17) and instruction(8);
+	no	<=	instruction(17) and instruction(7);
+
+	loadPc  <=  '1' when (instruction(17)='1' and instruction(2 downto 0)="001" and (ng='0' and zr='0')) else
+		    '1' when (instruction(17)='1' and instruction(2 downto 0)="010" and (ng='0' and zr='1')) else
+		    '1' when (instruction(17)='1' and instruction(2 downto 0)="011" and (ng='0'           )) else
+		    '1' when (instruction(17)='1' and instruction(2 downto 0)="100" and (ng='1' and zr='0')) else
+		    '1' when (instruction(17)='1' and instruction(2 downto 0)="101" and (	    zr='0')) else
+		    '1' when (instruction(17)='1' and instruction(2 downto 0)="110" and (ng='1'           )) else
+		    '1' when (instruction(17)='1' and instruction(2 downto 0)="111") 			     else 
+		    '0';
+
+
+
 
 
 end architecture;
