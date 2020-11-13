1813eff2f38ecdb6b53eb1101684c7812f689b4e
leonardodma <leonardodma@al.insper.edu.br>
2020-11-13 10:53:29 -0300

    Comp pronto e passando nos testes

--- Projetos/G-Assembler/Assembler/src/main/java/assembler/Code.java
+++ Projetos/G-Assembler/Assembler/src/main/java/assembler/Code.java
@@ -17,16 +17,70 @@ public class Code {
      */
     public static String dest(String[] mnemnonic) {
         /* TODO: implementar */
-        switch (mnemnonic[1]){
-            case "jmp": return "111";
-            case "jle": return "110";
-            case "jne": return "101";
-            case "jl": return "100";
-            case "jge": return "011";
-            case "je": return "010";
-            case "jg": return "001";
-            default: return "000";
+
+        if (!mnemnonic[0].equals("movw")) {
+            switch (mnemnonic[mnemnonic.length -1]) {
+                case "%A":
+                    return "0001";
+                case "%D":
+                    return "0010";
+                case "(%A)":
+                    return "0100";
+                default:
+                    return "0000";
+            }
         }
+        else {
+            if (mnemnonic.length > 3){
+                switch (mnemnonic[mnemnonic.length -1]){
+                    case "%A":
+                        if (mnemnonic[mnemnonic.length -2].equals("(%A)")){
+                            return "0101";
+                        }
+                        else if (mnemnonic[mnemnonic.length - 2].equals("%D")){
+                            return  "0011";
+                        }
+                        else {
+                            return "0000";
+                        }
+
+                    case "%D":
+                        if (mnemnonic[mnemnonic.length -2].equals("(%A)")){
+                            return "0110";
+                        }
+                        else if (mnemnonic[mnemnonic.length - 2].equals("%A")){
+                            return  "0011";
+                        }
+                        else {
+                            return "0000";
+                        }
+
+                    case "(%A)":
+                        if (mnemnonic[mnemnonic.length -2].equals("%A")){
+                            return "0101";
+                        }
+                        else if (mnemnonic[mnemnonic.length - 2].equals("%D")){
+                            return  "0110";
+                        }
+                        else {
+                            return "0000";
+                        }
+                }
+            }
+            else {
+                switch (mnemnonic[mnemnonic.length -1]) {
+                    case "%A":
+                        return "0001";
+                    case "%D":
+                        return "0010";
+                    case "(%A)":
+                        return "0100";
+                    default:
+                        return "0000";
+                }
+            }
+        }
+        return "0000";
     }
 
     /**
@@ -35,8 +89,92 @@ public class Code {
      * @return Opcode (String de 7 bits) com código em linguagem de máquina para a instrução.
      */
     public static String comp(String[] mnemnonic) {
-        /* TODO: implementar */
-    	return "";
+
+        int movInt = 0;
+
+        String comp = "";
+
+        if (mnemnonic[0].startsWith("mov")) {
+            comp = mnemnonic[1];
+            movInt = 1;
+        } else if (mnemnonic[0].startsWith("add")) {
+            comp = mnemnonic[1]+"+"+mnemnonic[2];
+        } else if (mnemnonic[0].startsWith("sub")) {
+            comp = mnemnonic[1]+"-"+mnemnonic[2];
+        } else if (mnemnonic[0].startsWith("rsub")) {
+            comp = mnemnonic[2]+"-"+mnemnonic[1];
+        } else if (mnemnonic[0].startsWith("inc")) {
+            comp = mnemnonic[1]+"+1";
+        } else if (mnemnonic[0].startsWith("dec")) {
+            comp = mnemnonic[1]+"-1";
+        } else if (mnemnonic[0].startsWith("not")) {
+            comp = "!"+mnemnonic[1];
+        } else if (mnemnonic[0].startsWith("neg")) {
+            comp = "-"+mnemnonic[1];
+        } else if (mnemnonic[0].startsWith("and")) {
+            comp = mnemnonic[1]+"&"+mnemnonic[2];
+        } else if (mnemnonic[0].startsWith("or")) {
+            comp = mnemnonic[1]+"|"+mnemnonic[2];
+        } else if (mnemnonic[0].startsWith("nop")) {
+            comp = "$0";
+        } else if ( mnemnonic[0].startsWith("jg")  ||
+                mnemnonic[0].startsWith("je")  ||
+                mnemnonic[0].startsWith("jge") ||
+                mnemnonic[0].startsWith("jl")  ||
+                mnemnonic[0].startsWith("jne") ||
+                mnemnonic[0].startsWith("jle") ||
+                mnemnonic[0].startsWith("jmp") )
+        {
+            if (mnemnonic.length == 1)
+                comp = "%X";
+            else
+                comp = mnemnonic[1];
+        }
+
+        String r2 = "0";     // Operacao entre S e D
+        String r1 = "0";     // A = 0; (A) = 1
+        String r0 = "0";     // A = 0; D = 1
+        boolean Xs = false;
+        boolean Xd = false;
+
+        for (int i = 0; i < mnemnonic.length - movInt; i++) {
+            if (mnemnonic[i].contains("(%A)")) {
+                r0 = "1";
+                break;
+            }
+        }
+
+        comp = comp.replace("(%A)", "%Y");
+        comp = comp.replace("%A", "%Y");
+        comp = comp.replace("%D", "%X");
+        comp = comp.replace("$", "");
+
+        String c = "000000";
+        switch (comp) {
+            case "0":    c = "101010"; break;
+            case "1":    c = "111111"; break;
+            case "-1":   c = "111010"; break;
+            case "%X":    c = "001100"; break;
+            case "%Y":    c = "110000"; break;
+            case "!%X":   c = "001101"; break;
+            case "!%Y":   c = "110001"; break;
+            case "-%X":   c = "001111"; break;
+            case "-%Y":   c = "110011"; break;
+            case "%X+1":  case "1+%X": c = "011111"; break;
+            case "%Y+1":  case "1+%Y": c = "110111";  break;
+            case "%X-1":  c = "001110"; break;
+            case "%Y-1":  c = "110010"; break;
+            case "%X+%Y": c = "000010"; break;
+            case "%Y+%X": c = "000010"; break;
+            case "%X-%Y": c = "010011"; break;
+            case "%Y-%X": c = "000111"; break;
+            case "%X&%Y": c = "000000"; break;
+            case "%Y&%X": c = "000000"; break;
+            case "%X|%Y": c = "010101"; break;
+            case "%Y|%X": c = "010101"; break;
+            default: c = "000000";
+        }
+        return r2 + r1 + r0 + c;
     }
 
     /**
@@ -65,7 +203,13 @@ public class Code {
      */
     public static String toBinary(String symbol) {
         /* TODO: implementar */
-    	return "";
+        // Convertendo a String para um valor inteiro
+        int symbol_value = Integer.parseInt(symbol);
+
+        // Passando para String o valor inteiro para binário
+        String binary = Integer.toBinaryString(symbol_value);
+
+        return String.format("%1$16s", binary).replace(" ", "0");
     }
 
 }
